da41f1b2906ecd68e5b72421e27a1619d0e601be
casey <caseywchristensen@gmail.com>
2024-09-08 20:36:37 -0600

    add ftc-lib
    Signed-off-by: casey <caseywchristensen@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBot.java
@@ -1,16 +1,39 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ProgrammingBot {
     private DigitalChannel touchSensor;
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private DifferentialDrive m_drive;
 
     public void init(HardwareMap hwMap){
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        leftMotor = hwMap.get(DcMotor.class, "Drive 1");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor = hwMap.get(DcMotor.class, "Drive 2");
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
+
     public boolean getTouchSensorState(){
         return touchSensor.getState();
+    }
+
+    public void runArcadeDrive(double forwardSpeed, double turnSpeed){
+        double speedAdjustment = 0.5;
+        leftMotor.setPower(forwardSpeed*speedAdjustment-turnSpeed*speedAdjustment);
+        rightMotor.setPower(forwardSpeed*speedAdjustment+turnSpeed*speedAdjustment);
+    }
+    public void runTankDrive(double leftSpeed, double rightSpeed){
+        leftMotor.setPower(leftSpeed);
+        rightMotor.setPower(rightSpeed);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveOpMode.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBot;
+
+@TeleOp(name = "Drive")
+public class DriveOpMode extends OpMode {
+    ProgrammingBot bot = new ProgrammingBot();
+    @Override
+    public void init(){
+        bot.init(hardwareMap);
+    }
+    @Override
+    public void loop(){
+        bot.runArcadeDrive(gamepad1.right_stick_y, gamepad1.right_stick_x);
+        //bot.setLeftMotorSpeed(gamepad1.left_stick_y);
+        //bot.setRightMotorSpeed(gamepad1.right_stick_y);
+    }
+}
