e8501378197bd2ebddae3309bb4acb7a7856f653
casey <caseywchristensen@gmail.com>
2024-09-17 15:39:40 -0600

    Add drive and claw op mode
    Signed-off-by: casey <caseywchristensen@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBot.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -15,6 +19,8 @@ public class ProgrammingBot {
     private DcMotor rightMotor;
     public double ticksPerRotation;
     private Servo servo;
+    boolean wasUp, wasDown;
+    int driveModeNum = 0;
 
     public void init(HardwareMap hwMap){
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
@@ -47,6 +53,37 @@ public class ProgrammingBot {
     public void runTankDrive(double leftSpeed, double rightSpeed){
         leftMotor.setPower(leftSpeed*SPEED_ADJUSTMENT);
         rightMotor.setPower(rightSpeed*SPEED_ADJUSTMENT);
+    }
+
+    public void driveBot(Gamepad gamepad1){ //needs inputs
+
+        if (gamepad1.dpad_up && !wasUp) {
+
+            driveModeNum--;
+            if (driveModeNum < 0) {
+                driveModeNum = 1;
+            }
+        }
+        wasUp = gamepad1.dpad_up;
+        if (gamepad1.dpad_down && !wasDown){
+            driveModeNum++;
+            if (driveModeNum > 1) {
+                driveModeNum = 0;
+            }
+        }
+        wasDown = gamepad1.dpad_down;
+
+        telemetry.addLine("Use Up and Down on D-pad to cycle through choices");
+        switch (driveModeNum){
+            case 0:
+                runArcadeDrive(gamepad1.right_stick_y, gamepad1.right_stick_x);
+                telemetry.addLine("Drive Mode: Arcade");
+                break;
+            case 1:
+                runTankDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
+                telemetry.addLine("Drive Mode: Tank");
+                break;
+        }
     }
 
     public double getLeftMotorRotations() {
