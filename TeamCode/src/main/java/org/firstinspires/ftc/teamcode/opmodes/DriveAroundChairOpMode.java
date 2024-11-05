853ef1fb9b681e562b92679ec8d852bedd1cb1bb
ParCinG <turtloids.turtle@gmail.com>
2024-11-05 14:12:36 -0700

    Elias Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveAroundChairOpMode.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+
+@Autonomous(name = "EliasAutonomous")
+public class DriveAroundChairOpMode extends OpMode {
+
+    MecanumDrive mecanumDrive = new MecanumDrive();
+
+    private boolean isGoing = true;
+
+    @Override
+    public void init() {
+        mecanumDrive.init(hardwareMap);
+    }
+    @Override
+    public void loop() {
+        if (isGoing) {
+            mecanumDrive.drive(0.5, 0.0, 0.0);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            mecanumDrive.drive(0.0, 0.5, 0.0);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            mecanumDrive.drive(-0.5, 0.0, 0.0);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            isGoing = false;
+        }
+    }
+}
