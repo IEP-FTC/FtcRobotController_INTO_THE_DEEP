6241eb3be6ac0c6ab2ac22f02c708e0d20e5e7e7
ParCinG <turtloids.turtle@gmail.com>
2024-12-10 15:23:25 -0700

    Add IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SimpleMecanumDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SimpleMecanumDriveOpMode.java
@@ -10,9 +10,6 @@ import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 public class SimpleMecanumDriveOpMode extends OpMode {
     MecanumDrive mecanumDrive = new MecanumDrive();
 
-    boolean wasUp, wasDown;
-    int driveModeNum = 0;
-
     @Override
     public void init() {
         mecanumDrive.init(hardwareMap);
@@ -23,33 +20,6 @@ public class SimpleMecanumDriveOpMode extends OpMode {
         double forward = gamepad1.left_stick_y;
         double right = gamepad1.left_stick_x;
         double rotate = gamepad1.right_stick_x;
-
-        if (gamepad1.dpad_up && !wasUp){
-            driveModeNum--;
-            if (driveModeNum < 0) {
-                driveModeNum = 1;
-            }
-        }
-        wasUp = gamepad1.dpad_up;
-        if (gamepad1.dpad_down && !wasDown){
-            driveModeNum++;
-            if (driveModeNum > 1) {
-                driveModeNum = 0;
-            }
-        }
-        wasDown = gamepad1.dpad_down;
-
-        telemetry.addLine("Use Up and Down on D-pad to cycle through choices");
-        switch (driveModeNum){
-            case 0:
-                mecanumDrive.drive(forward, right, rotate);
-                telemetry.addLine("Drive Mode: Normal");
-                break;
-            case 1:
-                mecanumDrive.driveGyroCorrected(forward, right, rotate);
-                telemetry.addLine("Drive Mode: Gyro Corrected");
-                break;
-        }
-
+        mecanumDrive.drive(forward, right, rotate);
     }
 }
