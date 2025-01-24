1eff18e5aa07c8515d831318e31a8c026b03e599
bennett <bennettjswenson@gmail.com>
2025-01-24 13:58:52 -0700

    bennett's edited code and new autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousSample.java
@@ -1,21 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmodes;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.teamcode.mechanisms.ArmPivot;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+import org.firstinspires.ftc.teamcode.mechanisms.Slide;
+
 
 @Autonomous(preselectTeleOp = "MasterMode")
-public class AutonomousSpecimen extends OpMode {
+public class AutonomousSample extends OpMode{
     MecanumDrive mecanumDrive = new MecanumDrive();
     ArmPivot armPivot = new ArmPivot();
+    Slide slide = new Slide();
     private DigitalChannel touchSensor;
     private enum Steps{
         DriveBackward,
-        Extend,
-        Hook,
+        RotateAndExtend,
+        Drop,
         NetZone
 
     }
@@ -26,53 +28,29 @@ public class AutonomousSpecimen extends OpMode {
         armPivot.init(hardwareMap);
         touchSensor = hardwareMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
-
+        slide.init(hardwareMap);
     }
 
     public void loop(){
         switch(step){
             case DriveBackward:
-                if(!touchSensor.getState()) {
-                    mecanumDrive.drive(-1, 0, 0);
+                if(!touchSensor.getState()){
+                    mecanumDrive.drive(-1,0,0);
                 } else {
                     mecanumDrive.stop();
-                    step = Steps.Extend;
+                    step = Steps.RotateAndExtend
                 }
-
-            case Extend:
-                armPivot.moveToPosition(90); //TEST edit the angle on this
-                //rotate the arm to an angle so as to put the thing on
-                //is extension required? test
-                step = Steps.Hook;
-            case Hook:
-                mecanumDrive.drive(1,0,0);
+            case RotateAndExtend:
+                mecanumDrive.drive(0,0,1); //TEST do we need to change this?
                 try {
                     Thread.sleep(500); //TEST edit the timing on this
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 mecanumDrive.stop();
-                step = Steps.NetZone;
-            case NetZone:
-                mecanumDrive.drive(1,0,0);
-                try {
-                    Thread.sleep(500); //TEST edit the timing on this
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                mecanumDrive.drive(1,0,0);
-                try {
-                    Thread.sleep(500); //TEST edit the timing on this
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                mecanumDrive.stop();
-
-
+                //extension code here
 
         }
-
-
 
     }
 }
