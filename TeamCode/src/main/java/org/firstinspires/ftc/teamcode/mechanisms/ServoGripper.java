94a8c19ef6b0e3809f66825c86b618bb9866179f
bennett <bennettjswenson@gmail.com>
2025-02-11 15:24:36 -0700

    mastermode arm reset update -Bennett

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ServoGripper.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ServoGripper {
+
+    private Servo intakeServoLeft;
+    private Servo intakeServoRight;
+
+    public void init(HardwareMap hardwareMap) {
+
+        intakeServoLeft = hardwareMap.get(Servo.class, "servoLeft");
+        intakeServoRight = hardwareMap.get(Servo.class, "servoRight");
+    }
+
+    public void open(){
+        intakeServoLeft.setPosition(0);
+    }
+    public void close(){
+        intakeServoLeft.setPosition(1);
+    }
+
+
+    public void addTelemetry(Telemetry telemetry){
+        telemetry.addData("Left Servo Position:", intakeServoLeft.getPosition());
+        telemetry.addData("Right Servo Position:", intakeServoRight.getPosition());
+    }
+
+}
+
